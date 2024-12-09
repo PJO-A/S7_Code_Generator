38103b56aad48545c232347c0c2ac471cb97d84f
PJO-A <info@pjo-automation.co.uk>
2024-12-09 08:40:49 +0000

    https://www.youtube.com/watch?v=y1SRu0IsdqU

---
+++ OpennessGenerator/Constructor/Controller.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using listbox = System.Windows.Controls.ListBox;
+
+
+namespace OpennessGenerator.Constructor
+{
+    public class Controller
+    {
+        public struct _Device
+        {
+            public string name;
+            public string identifier;
+            public string IP;
+            public _Device(string _name, string _identifier, string _IP)
+            {
+                name = _name;
+                identifier = _identifier;
+                IP = _IP;
+            }
+        }
+        public struct _IoCard
+        {
+            public string name;
+            public string position;
+            public string identifier;
+            public _IoCard(string _name, string _position, string _identifier)
+            {
+                name = _name;
+                position = _position;
+                identifier = _identifier;
+            }
+        }
+
+        public static IList<Tuple<_Device, IList<_IoCard>>> Device;
+        public static listbox lbMessage;
+
+        public static void ReadSettings(listbox _lbMessage)
+        {
+            lbMessage = _lbMessage;
+            string appBaseDir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+            string filename = appBaseDir + "\\controller.csv";
+            if (File.Exists(filename))
+            {
+                lbMessage.Items.Insert(0, DateTime.Now.ToString() + " Could not locate File: " + filename);
+                return;
+            }
+            try
+            {
+                var reader = new StreamReader(filename);
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    IList<_IoCard> iOC = new List<_IoCard>();
+                    var holdD = new _Device(values[0], values[1], values[2]);
+                    for (int i = 1; i < values.Length / 2; i++) 
+                    { 
+                        var holdIO  = new _IoCard(values[i*2+1], values[i*2+2], i.ToString());
+                        iOC.Add(holdIO);
+                    }
+                    Device.Add(new Tuple<_Device, IList<_IoCard>>(holdD, iOC));
+                }
+                lbMessage.Items.Insert(0, DateTime.Now.ToString() + "File objects.csv was read ");
+            }
+            catch (Exception ex)
+            {
+                lbMessage.Items.Insert(0, DateTime.Now.ToString() + " " + ex.Message);
+            }
+        }
+    }
+}
--- OpennessGenerator/MainWindow.xaml.cs
+++ OpennessGenerator/MainWindow.xaml.cs
@@ -1,14 +1,86 @@
 ﻿using OpennessGenerator.Constructor;
 using System.Windows;
+using Siemens.Engineering;
+using Siemens.Engineering.Hmi;
+using Siemens.Engineering.HW;
+using Siemens.Engineering.HW.Features;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+
+
 
 namespace OpennessGenerator
 {
     public partial class MainWindow : Window
     {
+        #region Field
+        private TiaPortal portal = null;
+        private Project project = null; 
+        private Device device = null;
+        private DirectoryInfo path = null;
+        private IoSystem system = null;
+        private IList<Device> subDevice = null;
+        private IList<Device> Devices = null;
+        private IList<addDevice> addDev = new List<addDevice>();
+        private addDevice add;
+
+        public struct addDevice
+        {
+            public string name;
+            public string author;
+            public string ipAddress;
+            public string identifier;
+            public string comment;
+        }
+
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
             ObjectIdentifier.ReadSettings(lbMessage);
+            Controller.ReadSettings(lbMessage);
+
+            tbPlcName.Text = "PLC_ofmSystems";
+            tbPlcAuthor.Text = "ofmSystems";
+            tbPlcIPAddress.Text = "192.168.10.20";
+            tbPlcIdentifier.Text = "PLC_CPU";
+
+            tbHmiName.Text = "PLC_ofmSystems";
+            tbHmiAuthor.Text = "ofmSystems";
+            tbPlcIPAddress.Text = "192.168.10.20";
+            tbPlcIdentifier.Text = "PLC_CPU";
+
+            tbComment.Text = "Standard comment";
+            tbPath.Text = "C:\\Users\\TIA V19\\source\\repos\\OpennessGenerator\\Siemens";
+            cbDelete.IsChecked = true;
+
+        }
+        private void createDevices(Project project)
+        {
+            Devices = new List<Device>();
+            NetworkInterface network = null;
+
+            add = new addDevice
+            {
+                name = tbHmiName.Text,
+                author = tbHmiAuthor.Text,
+                ipAddress = tbHmiIPAddress.Text,
+                identifier = tbHmiIdentifier.Text,
+                comment = tbComment.Text,
+            };
+            addDev.Add(add);
+
+            add = new addDevice
+            {
+                name = tbPlcName.Text,
+                author = tbPlcAuthor.Text,
+                ipAddress = tbPlcIPAddress.Text,
+                identifier = tbPlcIdentifier.Text,
+                comment = tbComment.Text,
+            };
+            addDev.Add(add);
         }
 
         private void btnGenHw_Click(object sender, RoutedEventArgs e)
@@ -19,6 +91,7 @@ namespace OpennessGenerator
         private void btnReloadCsv_Click(object sender, RoutedEventArgs e)
         {
             ObjectIdentifier.ReadSettings(lbMessage);
+            Controller.ReadSettings(lbMessage);
         }
     }
 }
